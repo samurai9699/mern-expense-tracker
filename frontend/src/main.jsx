b80a006b6a9e17aa9e21f1a80cbbaa6d1664c56e
sam muriithi <samuelmuriithi965@gmail.com>
2024-05-10 00:30:20 +0300

    dependancies added

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -2,9 +2,24 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
+import { BrowserRouter } from 'react-router-dom'
+import GridBackground from "./components/ui/GridBackground.jsx";
+import { ApolloClient, InMemoryCache, ApolloProvider} from '@apollo/client';
 
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+const client = new ApolloClient({
+	uri: 'http://localhost:4000/graphql', //url of our graphql server
+	cache: new InMemoryCache(), //Apollo client uses to cache query results after fetching them
+	credentials: 'include', //this tells apollo client to send cookies along with every request to the server
+  });
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+	<React.StrictMode>
+		<BrowserRouter>
+			<GridBackground>
+				<ApolloProvider client={client}>
+				<App />
+				</ApolloProvider>
+			</GridBackground>
+		</BrowserRouter>
+	</React.StrictMode>
+);
